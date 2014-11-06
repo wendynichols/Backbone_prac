ad3363e45dc3efadb85cec4ee70463a080258f30
Wendy Nichols <wendlelyn@gmail.com>
2014-11-06 15:43:43 -0500

    added file, update paths

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,73 +1,180 @@
-    var Feeling = Backbone.Model.extend({
+
+  (function () {
+
+    App.Models.Hero = Backbone.Model.extend({
 
       defaults: {
         title: '',
-        cause: '',
+        power: '',
         type: ''
       },
 
       idAttribute: '_id',
 
       initialize: function() {
-        var feel = this.get('title');
+        var hero = this.get('title');
       }
 
     });
 
-      var Feelings = Backbone.Collection.extend({
-      model: Feeling,
-      url: 'http://tiy-atl-fe-server.herokuapp.com/collections/backbonesecondpractice'
+  }());
+
+
+    (function () {
+
+      App.Collections.Heroes = Backbone.Collection.extend({
+      model: App.Models.Hero,
+      url: 'http://tiy-atl-fe-server.herokuapp.com/collections/superhero'
     });
 
-  var FeelingsView = Backbone.View.extend ({
+    }());
+
+  (function () {
+
+  App.Views.HeroesView = Backbone.View.extend ({
 
       tagName: 'ul',
-      className: 'feels',
+      className: 'hero',
 
-      initialize: function (options) {
-        this.render(options.collection);  //we've made the data available
+      events: {
+        'click li' : 'deleteMyHero'
       },
 
-      render: function (collection) {
+      initialize: function () {
+        this.render();  //we've made the data available
+
+        App.all_heroes.on('sync', this.render, this);
+        App.all_heroes.on('destroy', this.render, this);
+
+        $('#superheroContainer').html(this.el);
+
+      },
+
+      render: function () {
           //Binding 'this' to 'self' for use in nested
           // functions/callbacks
         var self = this;
 
 
           // Straight up underscore template goodness
-        var template = $('#feels').html();
+        var template = $('#hero').html();
         var rendered = _.template(template);
 
+
+        this.$el.empty();
+
           // Iterating over our models
-        _.each(collection.models, function (c) {
+        _.each(App.all_heroes.models, function (c) {
 
             // Each iteration...appending the data to
-            //    our element tht Backbone created
+            //    our element that Backbone created
           self.$el.append(rendered(c.attributes));
         });
 
 
-            // Take the data and append it into a
-            // specific element on my page
-        $('#feelsContainer').html(this.el);
-
-
         return this;
+
+      },
+
+        deleteMyHero: function (e) {
+          e.preventDefault();
+
+        var id = $(e.target).attr('id');
+        var goodbye = App.all_heroes.get(id);
+          goodbye.destroy();
+        }
+    });
+  }());
+
+(function () {
+
+    App.Views.HeroesAdd = Backbone.View.extend({
+      el: '#heroesAdder',  //from new div in index.html
+
+      events: {
+        'submit #heroesAdd' : 'addNewHero'   //A delegated event, has to be inside of our view element
+      },
+
+
+      initialize: function () {
+        this.render();
+      },
+
+      render: function () {
+        this.$el.html($('#addHeroes').html());
+      },
+
+        //Custom created elements after initialize and render
+      addNewHero: function (e) {
+        e.preventDefault();
+
+          // Grab feel values from my form
+        var heroes_title = $('#title').val();
+        var heroes_power = $('#power').val();
+        var heroes_type = $('#type').val();
+
+          // Create new Feeling
+        var hero = new App.Models.Hero({
+          title: heroes_title,
+          power: heroes_power,
+          type: heroes_type
+        });
+
+          // Add to our Collection and save to the server
+        App.all_heroes.add(hero).save();
+
+          // Clear my form
+        $('#heroesAdd')[0].reset();
 
       }
 
 
     });
 
+  }());
+
+  (function () {
+
+    App.Routers.AppRouter = Backbone.Router.extend({
+
+      initialize: function () {
+        // Light the Fire
+        Backbone.history.start();
+      },
+
+      routes: {
+        '' : 'home',
+        'edit/:id' : 'editCoffee'
+      },
+
+      home: function () {
+        new App.Views.AddHero();
+        new App.Views.ListHero({ collection: App.heroes });
+      },
+
+      editHero: function (id) {
+
+        var h = App.heroes.get(id);
+
+        new App.Views.SingleHero({ hero: h });
+      }
+
+    });
+
+  }());
+
+
+    (function () {
+
+      new App.Views.HeroesAdd();
 
       // Create instance of Feeling Collection
-    var all_feelings = new Feelings();
+    App.all_heroes = new App.Collections.Heroes();
 
 
       // Pull our feelingsfrom our server
-    all_feelings.fetch().done( function() {
-      var feelsview = new FeelingsView({
-        collection: all_feelings
+    App.all_heroes.fetch().done( function() {
+      new App.Views.HeroesView
       });
 
-    });
+    }());
