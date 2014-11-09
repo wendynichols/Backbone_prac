b25ec86fb81d31554e67b3e49e8e861dc5fdb1b7
Wendy Nichols <wendlelyn@gmail.com>
2014-11-09 18:46:42 -0500

    added view

--- app/scripts/views/heroes_add_view.js
+++ app/scripts/views/heroes_add_view.js
@@ -23,11 +23,13 @@
           // Grab feel values from my form
         var heroes_title = $('#title').val();
         var heroes_power = $('#power').val();
+        var heroes_alias = $('#alias').val();
 
           // Create new Feeling
         var hero = new App.Models.Hero({
           title: heroes_title,
           power: heroes_power,
+          alias: heroes_alias,
         });
 
           // Add to our Collection and save to the server
