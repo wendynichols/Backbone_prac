64636879ddaea9a9f599fa1bb600f6fb42e55dc6
Wendy Nichols <wendlelyn@gmail.com>
2014-11-09 19:00:16 -0500

    added collection

--- app/scripts/collections/superhero_collections.js
+++ app/scripts/collections/superhero_collections.js
@@ -3,6 +3,9 @@
 
       App.Collections.Heroes = Backbone.Collection.extend({
       model: App.Models.Hero,
+      comparator: function (model) {
+        return -parseInt(model.get('rating'));
+      },
       url: 'http://tiy-atl-fe-server.herokuapp.com/collections/superhero'
     });
 
