f642a3dfd6c019133b11bfefa855429548c4a9f3
Wendy Nichols <wendlelyn@gmail.com>
2014-11-09 19:14:54 -0500

    fixed main.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,15 @@
 
-    (function () {
+  (function () {
 
-      new App.Views.HeroesAdd();
+    // Create Instance of Collection
+    App.heroes = new App.Collections.Heroes();
 
-      // Create instance of Feeling Collection
-    App.all_heroes = new App.Collections.Heroes();
+    // Fetch any server-side coffees
+    App.heroes.fetch().done( function () {
+
+      App.router = new App.Routers.AppRouter();
+
+    });
 
 
-      // Pull our feelingsfrom our server
-    App.all_heroes.fetch().done( function() {
-      new App.Views.HeroesView
-      });
-
-    }());
+  }());
